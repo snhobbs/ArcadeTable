d50a85674965d97491e9e2de88e84a4b3be1852c
Simon <simon.hobbs@electrooptical.net>
2017-12-19 19:13:17 -0500

    done switch interrupts

--- switches.py
+++ switches.py
@@ -2,18 +2,86 @@
 import RPi.GPIO as GPIO
 import time
 import threading
+import subprocess
+import os
 
-keySwitch = 0
-startBtn = 1
-switchA = 2
-switchB = 2
-switchC = 2
+'''
+https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+'''
+pinKey = 1
+pinStart = 2
+pinA = 3
+pinB = 4
+pinC = 5
 
-GPIO.setmode(GPIO.BOARD)
+class Process(object):
+    def __init__(self, cmd, args = []):
+        self.cmd = cmd
+        self.args = args
 
-GPIO.setwarnings(False)
-GPIO.setup([keySwitch, startBtn, switchA, switchB, switchC], 
-        GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    def call(self):
+        subprocess.call([self.cmd] + self.args)
 
+class Switch(object):
+    def __init__(self, pin, process, direction = GPIO.IN, edgeMode = GPIO.FALLING, resMode = GPIO.PUD_UP):
+        self.pin = pin
+        self.process = process
+        self.direction = direction
+        self.edgeMode = edgeMode
+        self.resMode = resMode
 
-GPIO.add_event_detect(BTN_B, GPIO.BOTH, handle)
+    def setup(self):
+        GPIO.setup(
+            [self.pin], 
+            self.direction, 
+            pull_up_down = self.resMode,
+            bouncetime=200
+        )
+        GPIO.add_event_detect(
+            self.pin, 
+            self.edgeMode, 
+            call_back = self.process.call
+        )
+
+class SwitchController(object):
+    def __init__(self, switches):
+        self.switches = switches
+    
+    def setup(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        for switch in self.switches:
+            switch.setup()
+    
+def run():
+    keySwitch = Switch(
+        pinKen,
+        Process('shutdown', ['--now']),
+    )
+    
+    startBtn = Switch(
+        pinStart,
+        Process('emulationstation')
+    )
+
+    switchA = Switch(
+        pinA,
+        Process('echo', ['switch A'])
+    )
+
+    switchB = Switch(
+        pinB,
+        Process('echo', ['switch B'])
+    )
+
+    switchC = Switch(
+        pinC,
+        Process('echo', ['switch C'])
+    )
+
+    cont = SwitchController([keySwitch, startBtn, switchA, switchB, switchC])
+
+    cont.setup()
+
+if __name__ == "__main__":
+    run()
